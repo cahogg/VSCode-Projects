98389c0f6e5ae439ff9acdc7f8fffa8f68d2a58b
cahogg <110802283+cahogg@users.noreply.github.com>
2024-04-04 23:04:50 -0700

    Playing with Slice
    Course assignment complete. Pushing commit.

--- slice.go
+++ slice.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 )
 
 func main() {
-	UStore := make([]int, 3)
+	//Technically, they wanted size three. I created a means of extending theoretically infinitely instead.
+	UStore := make([]*int, 0)
 
-	//i is used to designate a location for the user input to be placed.
-	for i := 0; i < len(UStore); i++ {
-		var UInt int
+	for {
+		var input string
 
-		fmt.Println("Please enter an integer to slice.", i+1)
-		fmt.Scanln(&UInt)
+		fmt.Println("Please enter an integer to add to the slice, or 'X' to stop:")
+		fmt.Scanln(&input)
+		/* is the break command to stop. Note that, if you rerun the program, you will
+		save the inputs in the slice. Using X to break the loop restores the slice to zero*/
+		if input == "X" {
+			break
+		}
+		//Converts string to int
+		num, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a valid integer or 'X' to stop.")
+			continue
+		}
 
-		UStore[i] = UInt
+		// Creates new variable for adding more integers.
+		newNum := num
+		// Some value appending
+		UStore = append(UStore, &newNum)
+
+		// Sorts the slice. This sucked.
+		sort.Slice(UStore, func(i, j int) bool {
+			return *UStore[i] < *UStore[j]
+		})
+
+		// This prints the updated slice, and seperates each on a new line for readability
+		fmt.Println("Updated slice:")
+		for _, ptr := range UStore {
+			fmt.Println(*ptr)
+		}
 	}
 
-	fmt.Println("Your slice!", UStore)
+	// This prints the final sorted slice
+	fmt.Print("Final sorted slice: ")
+	for _, ptr := range UStore {
+		fmt.Print(*ptr)
+	}
 }
